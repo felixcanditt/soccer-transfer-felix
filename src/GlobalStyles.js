8ede140dbf7ec8f9e5aac13b5842d7622caec133
Felix Canditt <felix.canditt@gmail.com>
2021-05-18 00:11:07 +0200

    homework done

---
+++ src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 1.125rem;
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  text-align: center;
+}
+`;
